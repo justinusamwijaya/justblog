74ce10e21d8b5af469847d2e1ec8d6792b6d24f5
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-07-05 08:00:18 +0700

    adding mlab into it

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes')
 const cors = require('cors')
-require('mongoose').connect('mongodb://localhost:27017/testBlog')
+require('mongoose').connect('mongodb://user1:12345a@ds145223.mlab.com:45223/blogaaaa')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
